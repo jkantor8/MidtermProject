a5ad28cabccce319629a4dc4b2b2c68da622a366
xanimal37 <hawkbit37@gmail.com>
2023-03-16 15:13:51 -0500

    configure hibernate, test User entity

--- JPASportsMarketplace/src/main/java/com/skilldistillery/sportswap/entities/User.java
+++ JPASportsMarketplace/src/main/java/com/skilldistillery/sportswap/entities/User.java
@@ -1,8 +1,78 @@
 package com.skilldistillery.sportswap.entities;
 
-public class User {
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
 	
+	public User() {}
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private boolean enabled;
+	private String role;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUserName(String username) {
+		this.username = username;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + username + ", enabled=" + enabled + ", role=" + role + "]";
+	}
 	
 	
 }
--- JPASportsMarketplace/src/test/java/com/skilldistillery/sportswap/entities/UserTest.java
+++ JPASportsMarketplace/src/test/java/com/skilldistillery/sportswap/entities/UserTest.java
@@ -1,6 +1,11 @@
 package com.skilldistillery.sportswap.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -10,25 +15,36 @@ import org.junit.jupiter.api.Test;
 
 class UserTest {
 
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	
+	private User user;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPASportsMarketplace");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_entity_mapping() {
+		assertNotNull(user);
+		assertEquals("admin",user.getUsername());
 	}
 
 }
