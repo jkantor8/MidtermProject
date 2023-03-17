0580c9fe9ed3f7e8c4b4fc6f5b66dd73a0a64885
Tom <tom.mcglone1@yahoo.com>
2023-03-17 12:52:27 -0400

    All Listing entities and Junit tests created

--- JPASportsMarketplace/src/main/java/com/skilldistillery/sportswap/entities/AgeGroup.java
+++ JPASportsMarketplace/src/main/java/com/skilldistillery/sportswap/entities/AgeGroup.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name= "age_group")
 public class AgeGroup {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
---
+++ JPASportsMarketplace/src/main/java/com/skilldistillery/sportswap/entities/DonateListing.java
@@ -0,0 +1,125 @@
+package com.skilldistillery.sportswap.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "donate_listing")
+public class DonateListing {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private boolean active;
+	
+	@CreationTimestamp
+	private LocalDateTime created;
+	
+	@UpdateTimestamp
+	private LocalDateTime updated;
+	
+	private LocalDateTime deactivated;
+	
+	@Column(name= "event_start")
+	private LocalDateTime eventStart;
+	
+	@Column(name= "event_end")
+	private LocalDateTime eventEnd;
+	
+	public DonateListing() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public LocalDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+
+	public LocalDateTime getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(LocalDateTime updated) {
+		this.updated = updated;
+	}
+
+	public LocalDateTime getDeactivated() {
+		return deactivated;
+	}
+
+	public void setDeactivated(LocalDateTime deactivated) {
+		this.deactivated = deactivated;
+	}
+	
+
+	public LocalDateTime getEventStart() {
+		return eventStart;
+	}
+
+	public void setEventStart(LocalDateTime eventStart) {
+		this.eventStart = eventStart;
+	}
+
+	public LocalDateTime getEventEnd() {
+		return eventEnd;
+	}
+
+	public void setEventEnd(LocalDateTime eventEnd) {
+		this.eventEnd = eventEnd;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DonateListing other = (DonateListing) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "DonateListing [id=" + id + ", active=" + active + ", created=" + created + ", updated=" + updated
+				+ ", deactivated=" + deactivated + ", eventStart=" + eventStart + ", eventEnd=" + eventEnd + "]";
+	}
+	
+	
+}
